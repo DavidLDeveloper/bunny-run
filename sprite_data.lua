9fcc7481748aba50f71389b8378379e9aa207680
DavidLDeveloper <bluedragon_1_4@yahoo.com>
2025-06-30 19:50:15 -0700

    Final food sprite placement

--- sprite_data.lua
+++ sprite_data.lua
@@ -1,6 +1,6 @@
 function initiate_sprites()
     return {
-        {x=48, y=110, size=8, spr=2,visible=true},
+        {x=48, y=110, size=8, spr=2,visible=true}, -- outside
         {x=54, y=110, size=8, spr=2,visible=true},
         {x=176, y=72, size=8, spr=1,visible=true},
         {x=264, y=48, size=8, spr=2,visible=true},
@@ -14,6 +14,16 @@ function initiate_sprites()
         {x=552, y=64, size=8, spr=2,visible=true},
         {x=592, y=64, size=8, spr=1,visible=true},
         {x=632, y=64, size=8, spr=2,visible=true},
-        {x=672, y=80, size=8, spr=2,visible=true},
+        {x=672, y=80, size=8, spr=2,visible=true}, --end
+        {x=760, y=96, size=8, spr=2,visible=true}, --cave
+        {x=848, y=87, size=8, spr=2,visible=true}, 
+        {x=848, y=55, size=8, spr=1,visible=true},
+        {x=896, y=39, size=8, spr=1,visible=true},
+        {x=896, y=71, size=8, spr=2,visible=true},
+        {x=960, y=71, size=8, spr=2,visible=true},
+        {x=952, y=16, size=8, spr=1,visible=true},
+        {x=992, y=16, size=8, spr=1,visible=true},
+        {x=984, y=111, size=8, spr=2,visible=true},
+        {x=1000, y=111, size=8, spr=2,visible=true},
     }
 end
